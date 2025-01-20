403606283cf4e8241780c883e6abddf0a6204c42
yoshizawa tomomi <202411ap01_yoshizawa@x-ide.sakura.ne.jp>
2025-01-20 12:46:44 +0900

    #33669　距離が「A」～「D」以外も含まれている。

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -40,7 +40,8 @@ function subStockView($param)
 					<th>距離</th>
 					<td>
 						<?php
-						for ($i = 0; $i < 27; $i++) {
+						//「A」～「D」のチェックボックスを表示に修正
+						for ($i = 0; $i < 4; $i++) {
 						?>
 							<input type="checkbox" name="sDistance[]" value="<?php print $i + 1; ?>" <?php for ($j = 0; $j < 4; $j++) {
 																											if ($param["sDistance"][$j] == $i + 1) print ' checked="checked"';
