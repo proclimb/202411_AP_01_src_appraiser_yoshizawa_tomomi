d4c1cbfcbd641d96d6f8fb78d6b755b575724909
yoshizawa tomomi <202411ap01_yoshizawa@x-ide.sakura.ne.jp>
2025-02-06 14:20:30 +0900

    #34809　FM画面時の部屋番号の遷移

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -45,7 +45,7 @@ function subArticle()
 		<input type="hidden" name="sPage" value="<?php print $sPage ?>" />
 		<input type="hidden" name="articleNo" />
 		<input type="hidden" name="sName" />
-		<input type="hidden" name="sRoom" />
+		<!--<input type="hidden" name="sRoom" /> 隠さないでデータを遷移する -->
 
 		<a href="javascript:form.act.value='articleEdit';form.submit();"><img src="./images/btn_enter.png"></a>
 
