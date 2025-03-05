0e08502fe1e64a6d09935b497007e2514e6eb96b
yoshizawa tomomi <202411ap01_yoshizawa@x-ide.sakura.ne.jp>
2025-03-05 12:44:03 +0900

    #36695　PDFファイルのみ更新

--- SRC/js/fmanager.js
+++ SRC/js/fmanager.js
@@ -51,7 +51,9 @@ function fnFManagerViewEditCheck() {
 	}
 
 	tmp = form.pdfFile.value;
-	if (!form.pdfNo.value && tmp.slice(-4) != '.pdf' && tmp.slice(-4) != '.PDF') {
+	if (
+		//!form.pdfNo.value && 　PDFファイルのみを指定
+		tmp.slice(-4) != '.pdf' && tmp.slice(-4) != '.PDF') {
 		alert('PDFファイルを指定してください');
 		return;
 	}
